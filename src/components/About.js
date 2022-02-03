ac491133d89c9dff53e0eb4399a2fa13d82a5a08
Matheus Benamor <matheusqueirozbenamor@gmail.com>
2022-02-03 16:27:07 +0000

    logo ok

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const about = () => {
     return (
      <div>
-     <p>About</p>
+     <p>About us</p>
      </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 import About from './About';
 import Ranking from './Ranking';
+import logo from '../logo.png';
 
 
 const navbar = () => {
     return (
         <nav className="nav-style">
         <ul>
+        <li><img src= {logo} className="nav-logo" alt="logo" /></li>
+        <li>List of cities</li>
         <li><Link to='./Ranking.js'>Ranking</Link></li>
+        <li>Comments/Tips</li>
         <li><Link to='./About.js'>About</Link></li>
       </ul>
       </nav>
-      /*<div>
-      <h1>This is a navbar</h1>
-          <p><Ranking/></p>
-          <p><About/></p>
-      </div>*/
+      
     )
   }
    
