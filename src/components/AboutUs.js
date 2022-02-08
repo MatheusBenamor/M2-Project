71b5d4b037f7f687b281527b040f5e9a95bfeb6c
Matheus Benamor <matheusqueirozbenamor@gmail.com>
2022-02-08 19:28:27 +0000

    about page done, missing CSS and Github link

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React from "react";
+import "./AboutUs.css";
+import matheus from "../matheus.png";
+import eduardo from "../eduardo.png";
 
 const info = () => {
-    return (
-        <div>
-    <h1>Matheus Queiroz</h1>
-    <h1>Eduardo Brandião</h1>
-       </div>
-    )
-}
+  return (
+    <div className="about-container">
+      <div>
+        <h2>Eduardo Brandião</h2>
+        <img src={eduardo} className="edu-img" alt="eduardo-img" />
+        <p>Data science professional with SAP integration, fullstack development e software engineering student</p>
+        <p>Link GitHub vai aqui href="https://github.com/ebrandiao"</p>
+      </div>
+
+      <div>
+        <h2>Matheus Benamor</h2>
+        <img src={matheus} className="mathe-img" alt="matheus-img" />
+        <p>Web Development student at Ironhack São Paulo. Brazilian from Brasília, but currently living in Portugal.</p>
+        <p> My passions are travel, watch series and play games.</p>
+         <p>Link GitHub vai aqui href="https://github.com/MatheusBenamor"</p>
+      </div>
+    </div>
+  );
+};
 
 export default info;
