41f7cbf158c12ef10b9f3f204a1e63f0f41575a3
Eduardo Brandiao <eduardo.brandiao@al.infnet.edu.br>
2022-02-15 21:54:42 -0300

    corusel ajuste partial

--- src/components/DestinationCard.js
+++ src/components/DestinationCard.js
@@ -9,7 +9,8 @@ const DestinationCard = ({ city, name, _id }) => {
         <div className="destination-card">
             <Link to={`/destination/${_id}`}>
                 <img src={city[0].image} alt={city[0].cityName} />
-                <h3>{city[0].cityName}</h3>
+                <h3>City: {city[0].cityName}</h3>
+                <h3>Language: {city[0].language}</h3>
             </Link>
         </div>
     );
