5be008d3215c98b82ae84a29cda0ebe63641a0ca
Matheus Benamor <matheusqueirozbenamor@gmail.com>
2022-02-21 21:00:31 +0000

    improve navbar style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
-import logo from '../logo.png';
+import logoProject from '../logoProject.png';
 
 export const navbar = () => {
     return (
+      <>
         <nav className="nav-style">
-        
-        <p><NavLink to='/'><img src= {logo} className="nav-logo" alt="logo" /></NavLink></p>
-        <p><i><NavLink to='cities'>List of cities</NavLink></i></p>
-        <p><i><NavLink to='ranking'>Ranking</NavLink></i></p>
-        <p><i><NavLink to='comments'>Comments and Tips</NavLink></i></p>
-        <p><i><NavLink to='about'>About</NavLink></i></p>
-      
+        <ul>
+        <li><NavLink to='/'><img src= {logoProject} className="nav-logo" alt="logo" /></NavLink></li>
+        <li><p><i><NavLink to='cities'>List of cities</NavLink></i></p></li>
+        <li><p><i><NavLink to='ranking'>Ranking</NavLink></i></p></li>
+        <li><p><i><NavLink to='comments'>Comments and Tips</NavLink></i></p></li>
+        <li><p><i><NavLink to='about'>About</NavLink></i></p></li>
+        </ul>
       </nav>
-      
+      </>
     )
   }
    
